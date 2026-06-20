9ed140bedb99a8a6d109bff22d8646f267975908
pr0ch4kay <rbikovetsu@gmail.com>
2026-06-21 08:00:26 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,18 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': 'http://localhost:5000'
-    }
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // Это заставит Vite переименовывать CSS файл при каждой сборке, ломая кеш Vercel
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return `assets/style-[hash].css`;
+          }
+          return `assets/[name]-[hash].[ext]`;
+        },
+      },
+    },
+  },
 });
